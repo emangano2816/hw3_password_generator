dcf22cea989b38489b3451b29068320e0741625b
Elizabeth Mangano <79860046+emangano2816@users.noreply.github.com>
2021-05-07 15:26:55 -0400

    Added comments to explain process.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,11 @@
 
 // Assignment Code
-let generateBtn = document.querySelector("#generate");
-let passwordLength;
-let passwordCharacterOptions=[];
+let generateBtn = document.querySelector("#generate");  //Storing the button in a variable to call later
+let passwordLength;  //declaring variable to use in functions
+let passwordCharacterOptions=[]; //declaring variable to use in funcitons
 
 
+//Function returns passwordLength. To be used when generating the password
 function passwordLengthCriteria() {
   let lengthPrompt=prompt("Choose a password length between 8 and 156 characters.")
   passwordLength=Number(lengthPrompt); //converting string input to number to validate
@@ -20,18 +21,25 @@ function passwordLengthCriteria() {
   return passwordLength;
 }
 
+//Function returns passwordCharacterOptions
+//passwordCharacterOptions is an array that includes characters selected by the user to be used when generating the password
 function passwordCharacterCriteria() {
+  //Creating pop-up confirmation boxes to determine password criteria
   let includeLocaseAlpha=confirm("Would you like to include lower case alphas in your password?");
   let includeUpcaseAlpha=confirm("Would you like to include upper case alphas in your password?");
   let includeNumbers=confirm("Would you like to include numbers in your password?");
   let includeSymbols=confirm("Would you like to include symbols in your password");
+  //Creating criteriaCounter to validate that at least 1 criteria has been selected
   let criteriaCounter=0;
+  //Creating variables that house all the possible character types
   let loCaseAlphas=['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   let upCaseAlphas=['A','B','C', 'D','E','F','G','H','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   let numbers=['0','1','2','3','4','5','6','7','8','9'];
   let symbols=['~','`','@','#','$','%','^','&','*','(',')','_','-','+','=','{','}','[',']','|',':',';','"','<','>',',','.','?','/']
 
-
+  //Loading elements into the passwordCharacterOptions if the user has selected to include that character type
+  //If user clicks 'OK' that character type is put into the array and the criteriaCounter is incremented by 1
+  //If user clicks 'Cancel' that character type is not added to the array of options and the criteriaCounter stays the same
   if (includeLocaseAlpha===true) {
     criteriaCounter = ++criteriaCounter;
     passwordCharacterOptions=passwordCharacterOptions.concat(loCaseAlphas);
@@ -71,18 +79,27 @@ function passwordCharacterCriteria() {
 
   console.log(criteriaCounter);
 
+  //Checking that at least 1 character type has been include
+  //If the criteriaCounter is exactly equal to 0 then the user failed to select a charater type and will receive a message to start over
   if (criteriaCounter===0) {
     alert("Sorry, you must include at least 1 character type in your password.  Please try again.");
   } 
   return passwordCharacterOptions.length;
 }
 
+//generatePassword returns a password using the passwordLength and passwordCharacterOptions determined by the user
+//A password is returned when password criteria have been met; otherwise, the message "Your Secure Password" is returned
 function generatePassword() { 
-  passwordLengthCriteria();
-  
+  passwordLengthCriteria();  //calls passwordLengthCriteria function in order to determine password criteria
+
   if (passwordLength>=8 && passwordLength<=128 && passwordCharacterOptions !== 0){
-    
+    //Creating new array to store randomly selected elements
     const passwordArray = [];
+    
+    //Randomly selecting passwordLength characters from passwordCharachterOptions//
+    //For each i up to passwordLength, characters is set to a randomly selected array element from passwordCharacterOptions
+    //The array element is then pushed into passwordArray
+    //Once the for loop is complete, the function returns the elements of the passwordArray joined together as a string
     for (let i=0; i<passwordLength; i++){
       const characters = passwordCharacterOptions[Math.floor(Math.random() * passwordCharacterOptions.length)]
       passwordArray.push(characters);
@@ -93,16 +110,13 @@ function generatePassword() {
   }
 }
 
-
-
-
-
-// Write password to the #password input
+// writePassword  sets #password input to the value returned by generatePassword
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  
+  var password = generatePassword();                       //Password stores the result of the generatePassword function
+  var passwordText = document.querySelector("#password");  //passwordText stores the element used to display the password
 
-  passwordText.value = password;
+  passwordText.value = password;                           //Setting the value of the password display element to the generated password
 
 }
 
